952d1bd1218a3e6c15ee07f18da08c9d12dd3d9a
RP SOHAG <rpsohag.bd@gmail.com>
2023-07-01 10:50:20 +0600

    product filtering

--- Backend/controllers/ProductController.js
+++ Backend/controllers/ProductController.js
@@ -31,7 +31,46 @@ export const getSingleProduct = AsyncHandler(async (req, res) => {
 
 export const getAllProducts = AsyncHandler (async (req, res) => {
     try {
-        const products = await Product.find();
+
+        // filtering
+        const queryObj = {...req.query};
+        const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach(el => {
+            delete queryObj[el]
+        })
+
+        let queryString = JSON.stringify(queryObj);
+        queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+        let query = Product.find(JSON.parse(queryString));
+
+        // sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(" ")
+            query = query.sort(sortBy)
+        }else{
+            query = query.sort('-createdAt')
+        }
+
+        // limiting
+        if(req.query.fields){
+            const fields = req.query.fields.split(",").join(" ")
+            query = query.select(fields)
+        }else{
+            query = query.select("-__v")
+        }
+
+        // pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit)
+
+        if(req.query.page){
+            const productCount = await Product.countDocuments();
+            if(skip >= productCount) throw new Error("This is does not exits")
+        }
+
+        const products = await query;
         res.status(200).json({
             data: products
         })
