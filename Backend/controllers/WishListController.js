903f8adbbf18c4a38d5b13fe4d0ca5f05df9099c
RP SOHAG <rpsohag.bd@gmail.com>
2023-07-02 01:00:26 +0600

    add to wishlist

---
+++ Backend/controllers/WishListController.js
@@ -0,0 +1,28 @@
+import AsyncHandler from "express-async-handler";
+import User from "../models/User.js";
+
+
+
+export const addToWishList = AsyncHandler ( async (req, res) => {
+    const {_id} = req.user;
+    const {prodId} = req.body;
+    try {
+        const user = await User.findById(_id);
+        console.log(user)
+        const alreadyAddd = user.wishlist.find((id) => id.toString() === prodId.toString());
+        if(alreadyAddd){
+            let user = await User.findByIdAndUpdate(_id, {
+                $pull : { wishlist : prodId}
+            },{ new : true})
+            res.json(user)
+        }else{
+            let user = await User.findByIdAndUpdate(_id, {
+                $push : { wishlist: prodId}
+            },{ new : true})
+            res.json(user)
+        }
+
+    } catch (error) {
+        throw new Error(error)
+    }
+})
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { createProduct, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from '../controllers/ProductController.js';
 import { authMiddleware, checkIsAdmin } from '../middlewares/authMiddleware.js';
+import { addToWishList } from '../controllers/WishListController.js';
 const router = express.Router();
 
 router.post('/create', authMiddleware, checkIsAdmin, createProduct)
 router.get('/:id', authMiddleware, checkIsAdmin, getSingleProduct)
 router.get('/', authMiddleware, checkIsAdmin, getAllProducts)
 router.put('/update/:id', authMiddleware, checkIsAdmin, updateProduct)
+router.put('/wishlist', authMiddleware, addToWishList)
 router.delete('/delete/:id', authMiddleware, checkIsAdmin, deleteProduct)
 
 
