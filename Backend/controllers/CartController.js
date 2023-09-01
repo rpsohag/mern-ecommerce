0cef9a8f3c01cb30dbb17ce2de8aa2c6cb249313
RP SOHAG <rpsohag.bd@gmail.com>
2023-09-01 22:56:12 +0600

    show cart and delete all from cart

--- Backend/controllers/CartController.js
+++ Backend/controllers/CartController.js
@@ -1,46 +1,68 @@
-import AsyncHandler from 'express-async-handler';
-import User from '../models/User.js';
-import Cart from '../models/Cart.js';
-import Product from '../models/Product.js';
+import AsyncHandler from "express-async-handler";
+import User from "../models/User.js";
+import Cart from "../models/Cart.js";
+import Product from "../models/Product.js";
 
-export const userCart = AsyncHandler( async (req, res) => {
-    const { cart } = req.body;
-    const { _id } = req.user;
-    try {
-        let products = [];
-        const user = await User.findById(_id);
-        // check if user already have product in cart
-        const alreadyExistCart = await Cart.findOne({ orderby: _id });
-        if (alreadyExistCart) {
-          alreadyExistCart.remove();
-        }
-
-        for (let i = 0; i < cart.length; i++) {
-            let object = {};
-            object.product = cart[i]._id;
-            object.count = cart[i].count;
-            object.color = cart[i].color;
-            let getPrice = await Product.findById(cart[i]._id).select('price').exec();
-            object.price = getPrice.price;
-            products.push(object)
-        }
-        let cartTotal = 0;
-        for (let i = 0; i < products.length; i++) {
-            cartTotal = cartTotal + products[i].price * products[i].count;
-        }
-
-        
-        let newCart = await new Cart({
-            products,
-            cartTotal,
-            orderBy: user?._id
-        }).save()
-
-        return res.json({
-            newCart : newCart
-        })
-
-    } catch (error) {
-        throw new Error(error)
+export const userCart = AsyncHandler(async (req, res) => {
+  const { cart } = req.body;
+  const { _id } = req.user;
+  try {
+    let products = [];
+    const user = await User.findById(_id);
+    // check if user already have product in cart
+    const alreadyExistCart = await Cart.findOne({ orderby: _id });
+    if (alreadyExistCart) {
+      alreadyExistCart.remove();
     }
-})
+
+    for (let i = 0; i < cart.length; i++) {
+      let object = {};
+      object.product = cart[i]._id;
+      object.count = cart[i].count;
+      object.color = cart[i].color;
+      let getPrice = await Product.findById(cart[i]._id).select("price").exec();
+      object.price = getPrice.price;
+      products.push(object);
+    }
+    let cartTotal = 0;
+    for (let i = 0; i < products.length; i++) {
+      cartTotal = cartTotal + products[i].price * products[i].count;
+    }
+
+    let newCart = await new Cart({
+      products,
+      cartTotal,
+      orderBy: user?._id,
+    }).save();
+
+    return res.json({
+      newCart: newCart,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getUserCart = AsyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const cart = await Cart.findOne({ orderBy: _id }).populate(
+      "products.product"
+    );
+    res.json(cart);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const emptyCart = AsyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const user = await User.findOne(_id);
+    const cart = await Cart.findByIdAndRemove({ orderBy: user._id });
+
+    res.json(cart);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,24 +1,44 @@
-import express from 'express';
-import { adminLogin, blockUser, deleteSingleUser, forgotPasswordToken, getAllUser, getSingleUser, handleRefreshToken, resetPassword, unblockUser, updatePassword, updateSingleUser, useRegister, userLogin, userLogout } from '../controllers/UserController.js';
-import { authMiddleware, checkIsAdmin } from '../middlewares/authMiddleware.js';
-import { userCart } from '../controllers/CartController.js';
+import express from "express";
+import {
+  adminLogin,
+  blockUser,
+  deleteSingleUser,
+  forgotPasswordToken,
+  getAllUser,
+  getSingleUser,
+  handleRefreshToken,
+  resetPassword,
+  unblockUser,
+  updatePassword,
+  updateSingleUser,
+  useRegister,
+  userLogin,
+  userLogout,
+} from "../controllers/UserController.js";
+import { authMiddleware, checkIsAdmin } from "../middlewares/authMiddleware.js";
+import {
+  emptyCart,
+  getUserCart,
+  userCart,
+} from "../controllers/CartController.js";
 const router = express.Router();
 
-
-router.post('/register', useRegister);
-router.post('/login', userLogin)
-router.post('/admin-login', adminLogin)
-router.get('/users', getAllUser)
-router.post('/cart', authMiddleware, userCart)
-router.get('/user/:id', authMiddleware, checkIsAdmin, getSingleUser)
-router.delete('/user/:id', deleteSingleUser)
-router.put('/update/:id', authMiddleware, updateSingleUser)
-router.put('/block-user/:id', authMiddleware, checkIsAdmin, blockUser)
-router.put('/unblock-user/:id', authMiddleware, checkIsAdmin, unblockUser)
-router.post('/refresh-token', handleRefreshToken)
-router.put('/update-password', authMiddleware, updatePassword)
-router.post('/forgot-password-token', forgotPasswordToken)
-router.put('/reset-password/:token', resetPassword)
-router.get('/logout', userLogout)
+router.post("/register", useRegister);
+router.post("/login", userLogin);
+router.post("/admin-login", adminLogin);
+router.get("/users", getAllUser);
+router.post("/cart", authMiddleware, userCart);
+router.get("/cart", authMiddleware, getUserCart);
+router.delete("/empty-cart", authMiddleware, emptyCart);
+router.get("/user/:id", authMiddleware, checkIsAdmin, getSingleUser);
+router.delete("/user/:id", deleteSingleUser);
+router.put("/update/:id", authMiddleware, updateSingleUser);
+router.put("/block-user/:id", authMiddleware, checkIsAdmin, blockUser);
+router.put("/unblock-user/:id", authMiddleware, checkIsAdmin, unblockUser);
+router.post("/refresh-token", handleRefreshToken);
+router.put("/update-password", authMiddleware, updatePassword);
+router.post("/forgot-password-token", forgotPasswordToken);
+router.put("/reset-password/:token", resetPassword);
+router.get("/logout", userLogout);
 
 export default router;
